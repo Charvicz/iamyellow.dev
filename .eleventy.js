3674f60b483df32ed7ec6aa1afdc76a5fa3d43d9
Yellow <ondrej.charvat05@gmail.com>
2026-03-10 20:57:17 +0100

    add drone-promo passthrough + images

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site/assets");  // kopíruje VŠE včetně images + drone.css/js
   eleventyConfig.addPassthroughCopy({ "site/api": "api" });
   eleventyConfig.addWatchTarget("site/api/");
-  
+  eleventyConfig.addPassthroughCopy("site/drone-promo");  // kopíruje drone-promo do _site/drone-promo
+
   return {
     dir: {
       input: "site",
