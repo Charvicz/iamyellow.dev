6703d31da7773a2c847612af63b28edb6ebdce3d
Yellow <ondrej.charvat05@gmail.com>
2026-03-11 19:23:03 +0100

    add more drone photos + buildings

--- _site/assets/drone.js
+++ _site/assets/drone.js
@@ -244,3 +244,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// SMOOTH SCROLL pro tlačítka
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
+
